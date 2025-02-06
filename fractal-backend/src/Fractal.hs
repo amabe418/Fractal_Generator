18f1100d68d1ebfc99a2654010d935d063173ad3
ATKPowder <67365703+jotica0602@users.noreply.github.com>
2025-02-06 03:12:13 -0500

    Delete fractal-backend directory

--- fractal-backend/src/Fractal.hs
+++
@@ -1,9 +0,0 @@
-module Fractal 
-    ( module Fractal.Types
-    , module Fractal.Algorithms
-    , module Fractal.Generator
-    ) where
-
-import Fractal.Types
-import Fractal.Algorithms
-import Fractal.Generator
